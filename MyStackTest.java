b0d01fcdedca2ed810e4baac07480b9554a55fba
solitude6060 <solitude6060@gmail.com>
2015-04-20 20:57:25 +0800

    Test the program with entering five strings

--- MyStackTest.java
+++ MyStackTest.java
@@ -1,10 +1,25 @@
+
 import java.util.Scanner;
 public class MyStackTest {
 
 	public static void main(String[] args) {
 		MyStack<String> str =new MyStack<String>();
 		Scanner input = new Scanner(System.in);
+		String[] Chain = {"1","2","3","4","5"};
 		
+		System.out.println("Please enter five string into the program.");
+		
+		for(String i : Chain){
+			System.out.print("Please enter No."+i+" string : ");
+			str.push(input.next());
+		}
+		
+		System.out.println("");
+		System.out.println("Now displaying the Strings in reverse order.");
+		
+		while(!str.isEmpty()){
+			System.out.println(str.pop());;
+		}
 		
 		
 	}
